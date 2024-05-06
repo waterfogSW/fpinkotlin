e530bcd9865f1f21d21baded6ede1c4c82d36269
San Kim <waterfog9580@gmail.com>
2024-05-07 01:47:21 +0900

    ✨ Chapter 3(14)

--- src/test/kotlin/chapter3/exercises/ex14/listing.kt
+++ src/test/kotlin/chapter3/exercises/ex14/listing.kt
@@ -1,24 +1,25 @@
 package chapter3.exercises.ex14
 
+import chapter3.Cons
 import chapter3.List
+import chapter3.append
+import chapter3.foldRight
 import io.kotlintest.shouldBe
 import io.kotlintest.specs.WordSpec
 import utils.SOLUTION_HERE
 
 // tag::init[]
 fun <A> concat(lla: List<List<A>>): List<A> =
-
-    SOLUTION_HERE()
+    foldRight(lla, List.empty()) { cur, acc ->
+        foldRight(cur, acc) { icurr, iacc -> Cons(icurr, iacc) }
+    }
 
 fun <A> concat2(lla: List<List<A>>): List<A> =
+    foldRight(lla, List.empty()) { cur, acc -> append(cur, acc) }
 
-    SOLUTION_HERE()
-// end::init[]
-
-//TODO: Enable tests by removing `!` prefix
 class Exercise14 : WordSpec({
     "list concat" should {
-        "!concatenate a list of lists into a single list" {
+        "concatenate a list of lists into a single list" {
             concat(
                 List.of(
                     List.of(1, 2, 3),
