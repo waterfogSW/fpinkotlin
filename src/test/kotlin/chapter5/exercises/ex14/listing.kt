5835975f9963baa0102aff5dddb5f950d225bb38
San Kim <waterfog9580@gmail.com>
2024-05-19 23:47:13 +0900

    ✨ Chapter 5(14)

--- src/test/kotlin/chapter5/exercises/ex14/listing.kt
+++ src/test/kotlin/chapter5/exercises/ex14/listing.kt
@@ -1,26 +1,28 @@
 package chapter5.exercises.ex14
 
 import chapter5.Stream
+import chapter5.exercises.ex13.takeWhile
+import chapter5.exercises.ex13.zipAll
+import chapter5.exercises.ex4.forAll
 import io.kotlintest.shouldBe
 import io.kotlintest.specs.WordSpec
-import utils.SOLUTION_HERE
 
-//TODO: Enable tests by removing `!` prefix
 class Exercise14 : WordSpec({
 
     //tag::startswith[]
     fun <A> Stream<A>.startsWith(that: Stream<A>): Boolean =
-
-        SOLUTION_HERE()
+        this.zipAll(that)
+            .takeWhile { !it.second.isEmpty() }
+            .forAll { it.first == it.second }
     //end::startswith[]
 
     "Stream.startsWith" should {
-        "!detect if one stream is a prefix of another" {
+        "detect if one stream is a prefix of another" {
             Stream.of(1, 2, 3).startsWith(
                 Stream.of(1, 2)
             ) shouldBe true
         }
-        "!detect if one stream is not a prefix of another" {
+        "detect if one stream is not a prefix of another" {
             Stream.of(1, 2, 3).startsWith(
                 Stream.of(2, 3)
             ) shouldBe false
